12f2c4e025f14ef088af81e13235a91587e93b9d
Franco-Lobos <francolobos3@gmail.com>
2023-01-08 23:06:27 +0100

    switchTesting

--- js/Screen.js
+++ js/Screen.js
@@ -1,15 +1,49 @@
 class Screen {
-    constructor(data){
+    constructor(dim=10){
         this.dim = dim;
         this.matrix=[];
         this._setMatrix();
+        // this._switcher([0,0],[10,10],1)
     }
   
     _setMatrix(){
-        this.matrix.push(this.dim*[this.dim*[0]]);
-  
+        for(let i= 0; i< this.dim; i++){
+            this.matrix.push([]);
+            for(let j= 0; j< this.dim; j++){
+                this.matrix[i].push(0);
+            }
+        }
+    }
+
+    _switch(bool){
+        if(bool){
+            return 0;
+        }
+        return 1;
+    }
+
+    _switcher(vect1,vect2, value){
+        let x1 = vect1[0];
+        let y1 = vect1[1];
+        let x2 = vect2[0];
+        let y2 = vect2[1];
+
+        for(let i = x1; i<= x2; i++){
+            for(let j = y1; j<= y2; j++){
+                this.matrix[i][j]= value!== undefined ? value: this._switch(this.matrix[i][j]);
+            }
+        }
+    }
+    
+    turnOn(vect1,vect2){
+        _switcher(vect1,vect2,1);
+    }
+
+    turnOf(vect1,vect2){
+        _switcher(vect1,vect2,0);
+    }
+    
+    switch(vect1,vect2){
+        _switcher(vect1,vect2);
     }
   }
-  
-  let screen = new Screen(10);
-  
--- test/stack.test.js
+++ test/stack.test.js
@@ -3,19 +3,53 @@ class Screen {
         this.dim = dim;
         this.matrix=[];
         this._setMatrix();
+        // this._switcher([0,0],[10,10],1)
     }
   
     _setMatrix(){
         for(let i= 0; i< this.dim; i++){
             this.matrix.push([]);
             for(let j= 0; j< this.dim; j++){
-                this.matrix[i].push([0]);
+                this.matrix[i].push(0);
             }
         }
     }
+
+    _switch(bool){
+        if(bool){
+            return 0;
+        }
+        return 1;
+    }
+
+    _switcher(vect1,vect2, value){
+        let x1 = vect1[0];
+        let y1 = vect1[1];
+        let x2 = vect2[0];
+        let y2 = vect2[1];
+
+        for(let i = x1; i<= x2; i++){
+            for(let j = y1; j<= y2; j++){
+                this.matrix[i][j]= value!== undefined ? value: this._switch(this.matrix[i][j]);
+            }
+        }
+    }
+    
+    turnOn(vect1,vect2){
+        _switcher(vect1,vect2,1);
+    }
+
+    turnOf(vect1,vect2){
+        _switcher(vect1,vect2,0);
+    }
+    
+    switch(vect1,vect2){
+        _switcher(vect1,vect2);
+    }
   }
-  
-  let screen = new Screen();
+
+//Testing
+let screen = new Screen();
   
 describe('create matrix', ()=>{
 
@@ -42,10 +76,34 @@ describe('create matrix', ()=>{
     });
 
     it('all values are cero',()=>{
-        screen.matrix.forEach((element, indx) => {
-            element.forEach((array)=>{
-                expect(array).toEqual(expect.arrayContaining([0]));
+        screen.matrix.forEach((element) => {
+            element.forEach((item)=>{
+                expect(item).toEqual(0);
             })
         });
     });
+})
+
+
+describe('change light status', ()=>{
+    
+    let screen;
+
+    beforeEach(()=>{
+        screen = new Screen();
+    });
+
+    it('switcher', ()=>{
+        let dimIndx = screen.dim-1;
+        let defaultMatrix = new Screen().matrix;
+
+        screen._switcher([0,0],[dimIndx,dimIndx]);
+        defaultMatrix.forEach((x,xIndx)=>{
+            x.forEach((y)=>{
+                expect(screen.matrix[xIndx][y]).not.toEqual(defaultMatrix[xIndx][y]);
+            })
+        })
+
+    })
+
 })
